20c8c026d341bf034140a3290f804312b7c580ed
Shlomi Goland <golandsh@gmail.com>
2021-04-10 17:24:35 +0300

    Some fixes for completion

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/shlomi-test'),
+      dir: require('path').join(__dirname, './coverage/weather-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/shlomi-test'));
+app.use(express.static(__dirname + '/dist/weather-app'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/shlomi-test/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/weather-app/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
